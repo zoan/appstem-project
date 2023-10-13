ef4b56bafc2ba0b240135eb94a564cc0b10f9b79
Marc Wong <marcdwong@gmail.com>
2023-10-12 19:53:56 -0700

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,23 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from "next/image";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <form className="flex flex-col items-center justify-center" onSubmit={(e) => { alert('asdf') }}>
-      <label>Search</label>
-      <input />
-      <button type="submit">Submit</button>
+      <form
+        className="flex flex-col items-center justify-center"
+        onSubmit={(e) => {
+          alert("asdf");
+        }}
+      >
+        <label>Search</label>
+        <input />
+        <button type="submit">Submit!!</button>
       </form>
     </main>
-  )
+  );
 }
