7442c3b1e07d7c0b09f97911b85c28d829e940d3
Marc Wong <marcdwong@gmail.com>
2023-10-14 16:45:01 -0700

    refactor: update form styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -108,9 +108,20 @@ export default function Home() {
         <Image src="/appstem-logo.png" width={296} height={48} alt="Appstem logo" />
         {/* <p>Query: {query}</p> */}
         <form className="flex flex-col items-center justify-center" onSubmit={handleSubmit}>
-          <label>Search</label>
-          <input />
-          <button type="submit">Submit!!</button>
+          <div className="flex items-center border-b border-teal-500 py-2">
+            <input
+              className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+              type="text"
+              placeholder="Search images..."
+              aria-label="Full name"
+            />
+            <button
+              className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
+              type="submit"
+            >
+              Search
+            </button>
+          </div>
         </form>
         <div className="w-auto">
           {!images?.length ? null : (
