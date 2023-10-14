159f0e85582c212f1c6b7b112a2484d7890949c1
Marc Wong <marcdwong@gmail.com>
2023-10-14 12:35:21 -0700

    refactor: use env variable to get API Key

--- src/pages/api/images.ts
+++ src/pages/api/images.ts
@@ -6,6 +6,8 @@ import { PixabayGetResponse } from "@/utils/types";
 
 const router = createRouter<NextApiRequest, NextApiResponse>();
 
+const apiKey = process.env.PIXABAY_API_KEY;
+
 router.get(async (req, res) => {
   try {
     const { currentPage = 1, searchQuery = "", perPage = 20 } = req.query;
@@ -13,7 +15,7 @@ router.get(async (req, res) => {
     const formattedQuery = (searchQuery as string).replaceAll(" ", "+");
 
     const pixabay = await fetch(
-      `https://pixabay.com/api/?key=40014584-2010ba5ee0e4df4efd5fa33ec&q=${formattedQuery}&per_page=${perPage}&page=${currentPage}`
+      `https://pixabay.com/api/?key=${apiKey}&q=${formattedQuery}&per_page=${perPage}&page=${currentPage}`
     );
 
     const data = (await pixabay.json()) as PixabayGetResponse;
