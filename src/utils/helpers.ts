70c5002431fa60d9d3ea35ae24836747b63ca342
Marc Wong <marcdwong@gmail.com>
2023-10-14 14:08:29 -0700

    refactor: update helper fetch fn to use next api

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,15 +1,7 @@
 import { PixabayGetResponse } from '@/utils/types';
 
 export const fetchPixabay = async ({ query = '', currentPage = 1 }) => {
-  console.log(process.env.NEXT_PUBLIC_VERCEL_URL);
-  console.log(process.env.NEXT_PUBLIC_VERCEL_ENV);
-  // make sure query is url encoded and replace spaces with "+" as per the Pixabay Docs
-  // https://pixabay.com/api/docs/
-  const formattedQuery = query.replaceAll(' ', '+');
-
-  const pixabay = await fetch(
-    `https://pixabay.com/api/?key=40014584-2010ba5ee0e4df4efd5fa33ec&q=${formattedQuery}&per_page=20&page=${currentPage}`
-  );
+  const pixabay = await fetch(`/api/images?searchQuery=${query}&perPage=20&page=${currentPage}`);
 
   const data = (await pixabay.json()) as PixabayGetResponse;
 
